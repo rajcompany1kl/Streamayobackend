8c47fc5d3c596850a9f924d8132ac8302c6b083d
Abbas Ali Dalal <abbas.dalal19402@gmail.com>
2025-10-14 15:27:49 +0530

    established db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideosModule } from './app/modules/videos/videos.module';
+import { DatabaseModule } from './app/modules/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [VideosModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), VideosModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
